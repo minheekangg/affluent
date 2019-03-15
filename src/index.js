9117cc1be3c3b65b0441a28d25ad855b4cad3e23
minheekangg <minheekangg@gmail.com>
2019-03-15 16:11:36 -0400

    merge

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
     console.log('DOM Content Loaded')
     // const startHere = document.querySelector('#start-here')
 
-
+    // When the user scrolls the page, execute myFunction 
+    window.onscroll = function () { myFunction() };
+    // Get the navbar
+    var navbar = document.querySelector('nav');
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetTop;
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
 
 
 
