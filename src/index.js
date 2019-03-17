9593fe75cd026be55d486cb49dcbaf61fe7ff8ef
minheekangg <minheekangg@gmail.com>
2019-03-16 22:47:41 -0400

    toggle navbar

--- src/index.js
+++ src/index.js
@@ -5,17 +5,43 @@ document.addEventListener('DOMContentLoaded', ()=>{
     // When the user scrolls the page, execute myFunction 
     window.onscroll = function () { myFunction() };
     // Get the navbar
-    var navbar = document.querySelector('nav');
+    var navbar1 = document.querySelector('#nav1');
+    var navbar2 = document.querySelector('#nav2');
     // Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
+    var sticky = navbar1.offsetTop;
     // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
     function myFunction() {
         if (window.pageYOffset >= sticky) {
-            navbar.classList.add("sticky")
+            console.log(pageYOffset)
+            navbar1.classList.add("sticky")
+            navbar2.classList.add("sticky")
         } else {
-            navbar.classList.remove("sticky");
+            console.log(pageYOffset)
+            navbar1.classList.remove("sticky");
+            navbar2.classList.remove("sticky");
         }
     }
+
+    // CHANGE INDEX.HTML NAVBAR
+    var mainbottom =
+      $("#aboutpage-1").offset().top + $("#aboutpage-1").height();
+    var navbar1 = document.querySelector("#nav1");
+    var navbar2 = document.querySelector("#nav2");
+    // on scroll, 
+    $(window).on('scroll', function () {
+
+        // we round here to reduce a little workload
+        var stop = Math.round($(window).scrollTop());
+
+        if (stop > mainbottom) {
+            navbar1.classList.add("hidden");
+            navbar2.classList.remove("hidden");
+        } else {
+            navbar2.classList.add("hidden");
+            navbar1.classList.remove("hidden");
+        }
+
+    });
 
 
 
